a1ddcba19a49e77e00a10853f98b12868ab63d66
AnnaKonoval <konovala054@gmail.com>
2025-05-06 13:28:50 +0300

    complete lesson-2-2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -9,14 +9,25 @@
 
 let dice1 // your code
 let dice2 // your code
-let isWinningDouble // your code
+let isWinningDouble = false // your code
 
-/*
+while (!isWinningDouble) {
+  dice1 = Math.floor(Math.random() * 7)
+  dice2 = Math.floor(Math.random() * 7)
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
+
+if (dice1 === dice2 && dice1 === 3) {
+    isWinningDouble = true
+}
+
 if (isWinningDouble) {
   console.log('Выигрышный дубль!')
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
+
+
+
+}
