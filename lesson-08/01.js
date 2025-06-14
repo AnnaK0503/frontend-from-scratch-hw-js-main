1f403fcad26dc2b95214b629507701a9248a2509
AnnaKonoval <konovala054@gmail.com>
2025-06-14 13:15:11 +0300

    complete task 8-1

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,4 +6,18 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str) {
+  let words = str.split(' ');
+    let capitalizedWords = [];
+
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        // Делаем первую букву заглавной, остальные - строчными
+        capitalizedWords.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+    }
+
+    return capitalizedWords.join(' ');
+
+}
+
+console.log(capitalizeWords("hello world from javascript"));
